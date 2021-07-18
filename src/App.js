aa23e8c9c04f5b3294452a01192a17598f31253f
Gilbertelnino <gilbeltelnino@gmail.com>
2021-07-18 16:18:55 +0200

    complete search app frontend

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [photos, setPhotos] = useState([]);
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState();
+  // const [loading, setLoading] = useState(false);
+
+  const inputChange = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+
+  const fetchPhotos = async () => {
+    if (!search) {
+      setError("Input Can not be empty");
+      return setTimeout(() => setError(""), 5000);
+    }
+
+    const response = await fetch(
+      `https://search-pic-backend.herokuapp.com/api/albums/${search}/photos`
+    );
+
+    const { data } = await response.json();
+
+    if (data.length === 0) {
+      setPhotos([]);
+      return setError("Album with the given Id not found, Try Again!");
+    }
+    setError("");
+    return setPhotos(data);
+  };
+
+  return (
+    <div className="App container">
+      <h1>Search Photo app</h1>
+      <div className="form-search">
+        <input
+          type="number"
+          name="search"
+          placeholder="search photoes by id"
+          value={search}
+          onChange={inputChange}
+        />
+        <button onClick={fetchPhotos}>Get Album Photos By Id</button>
+      </div>
+      <div style={{ color: "red", fontSize: "1.5rem", textAlign: "center" }}>
+        {error && error}
+      </div>
+      <div className="images-container">
+        {photos?.map((photo) => (
+          <div className="content" key={photo.id}>
+            <img src={photo.thumbnailUrl} alt={photo.title} />
+            <p>{photo.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
